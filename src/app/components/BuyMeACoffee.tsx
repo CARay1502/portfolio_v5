34190bb8c7e30d41d53a6b1e9b3c0241ec83da96
Christian Ray <143613191+CARay1502@users.noreply.github.com>
2025-03-30 16:57:53 -0400

    Update BuyMeACoffee.tsx
    failed build log: 
    1:8  Error: 'Script' is defined but never used.  @typescript-eslint/no-unused-vars
    6:13  Error: Synchronous scripts should not be used. See: https://nextjs.org/docs/messages/no-sync-scripts  @next/next/no-sync-scripts
    
    removed unused script for embeded buymeacoffee link. utilized a href instead. Will create a pull request to update to <Link>

--- src/app/components/BuyMeACoffee.tsx
+++ src/app/components/BuyMeACoffee.tsx
@@ -3,18 +3,6 @@ import Script from "next/script";
 export default function BuyMeACoffee() {
     return (
         <div>
-            <script 
-            type="text/javascript" 
-            src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" 
-            data-name="bmc-button" 
-            data-slug="chris.is.coding" 
-            data-color="#5F7FFF" data-emoji=""  
-            data-font="Arial" 
-            data-text="Buy me a coffee" 
-            data-outline-color="#000000" 
-            data-font-color="#ffffff" 
-            data-coffee-color="#FFDD00" 
-        />
             
             <a className="bmc-button btn btn-accent text-lg font-bold btn-block hover:btn-neutral" href="https://www.buymeacoffee.com/chris.is.coding">
                 Buy Me a Coffee
